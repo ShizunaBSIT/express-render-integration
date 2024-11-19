bd80547bc6b60ac728bc7161f0d749f0f799b350
ShizunaBSIT <msyokozawa@tip.edu.ph>
2024-11-19 11:34:31 +0800

    update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/',(req,res) => {
-    res.send('Render Deployment Project');
-});
+app.use(express.json());
+
+
+app.use('/products',productService);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
